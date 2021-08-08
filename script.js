a76d7e9ab58504c66d07ce0848c15b81b66f1f2b
Johnsonha801 <Johnsonha801@gmail.com>
2021-08-07 21:57:46 -0600

    generatePassword function and variables.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 // Assignment Code
+
+// Generate Password Function
+var generatePassword = function() {
+  // establishing allowed character arrays
+  const lowerCaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const uppderCaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  
+  //Added duplicates of numeric values to even chances of being selected in concatinated array
+  const numericValues = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9]; 
+  const specialChars = ['!','"','#','$','&','%','^',"'",'(',')','*','+','-','.','<','>','?','/','[',']','{','}','|','~','_'];
+  var includedCharacters = [];
+  var generatedPass = '';
+  var passLength = 0;
+
+
+ // return the generated password
+ return generatedPass;
+}
+
+
+// references to the generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
