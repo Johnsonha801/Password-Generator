18958e4d3e85658dda3dc4361c50bff57323f28a
Johnsonha801 <Johnsonha801@gmail.com>
2021-08-08 19:12:29 -0600

    added password length, and had code to add all characters as well as lower and upper case letters

--- script.js
+++ script.js
@@ -13,11 +13,54 @@ var generatePassword = function() {
   var generatedPass = '';
   var passLength = 0;
 
+// Password Length
+passLength = parseInt(window.prompt("What length would you like the password to be? Must be between 8 and 128 characters."));
+while(isNaN(passLength) || passLength < 8 || passLength > 128) {
+  passLength = parseInt(window.prompt("You must enter a valid number between 8 and 128. Please try again."));
+}
+
+// Include Lowercase?
+var includeLower = window.confirm("Would you like lowercase characters to be included?");
+if(includeLower) {
+  includedCharacters = includedCharacters.concat(lowerCaseLetters);
+}
+
+// Include Uppercase?
+var includeUpper = window.confirm("Would you like uppercase characters to be included?");
+if(includeUpper) {
+  includedCharacters = includedCharacters.concat(uppderCaseLetters);
+}
+
+// Include Numeric?
+var includeNumeric = window.confirm("Would you like numeric values to be included?");
+if(includeNumeric) {
+  includedCharacters = includedCharacters.concat(numericValues);
+}
+
+// Include Special Characters? 
+var includeSpecial = window.confirm("Would you like special characters to be included?");
+if(includeSpecial) {
+  includedCharacters = includedCharacters.concat(specialChars);
+}
+ // Check if user selected at least one character type
+ if(includeLower || includeUpper || includeNumeric || includeSpecial) {
+  // Loop through and assign values to string
+  for(var i = 0; i < passLength; i++) {
+      // get random number out of the total includedCharacters total as highest value
+      var selectedChar = includedCharacters[Math.floor(Math.random() * includedCharacters.length)];
+      generatedPass = generatedPass + selectedChar; 
+  }
+
 
  // return the generated password
  return generatedPass;
-}
 
+
+} else {
+  // User did not select an option.
+  window.alert("You must include at least one character type. Start over and select at least one option.");
+  }
+}
 
 // references to the generate element
 var generateBtn = document.querySelector("#generate");
